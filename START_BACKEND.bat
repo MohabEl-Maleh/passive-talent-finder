1b6855b596540444389fe21d9c59a79018eb990f
MohabEl-Maleh <moelmaleh001@gmail.com>
2026-05-12 23:12:25 +0300

    Update START_BACKEND.bat

--- START_BACKEND.bat
+++ START_BACKEND.bat
@@ -10,8 +10,3 @@ call venv\Scripts\activate
 set PYTHONPATH=%CD%
 uvicorn main:app --reload --port 8000
 pause
-or just paste in terminal 1 (Backend):
-cd backend
-venv\Scripts\activate
-set GROQ_API_KEY=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
-uvicorn main:app --port 8000
